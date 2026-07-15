fd90bc6093bf44ac2fe008be3a6e2bc8d93d7a68
Jean-Michael <34538443+DO2JMG@users.noreply.github.com>
2026-07-15 18:53:16 +0200

    Update uploader.cpp

--- src/uploader.cpp
+++ src/uploader.cpp
@@ -8,7 +8,7 @@
 #include <regex>
 
 namespace {
-constexpr const char* APP_VERSION = "0.2.34";
+constexpr const char* APP_VERSION = "0.1.00";
 constexpr const char* TELEMETRY_URL = "http://api.wettersonde.net/telemetrie.php";
 constexpr const char* POSITION_URL = "http://api.wettersonde.net/position.php";
 
